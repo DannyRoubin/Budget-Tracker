ef6bd40a22cb6b0b31e641008a1c8735f83e6dab
Danny Roubin <dannyroubin1@gmail.com>
2021-08-08 18:17:32 -0700

    trying to get the service worker to work

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,37 @@ const config = {
     filename: "bundle.js",
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: "Budget Tracker Application",
+      short_name: "Budget App",
+      description: "An application that allows you to track budget items.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("./public/assets/icons/icon-192x192.png"),
+          sizes: [192, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
+  ],
 };
 module.exports = config;
